8279dd073444aba1bad91f49b81dd50cc258ddba
Jonathan Dorsey <jdorsey43@gatech.edu>
2025-10-11 21:12:26 -0700

    [testing] GTest & CTest working in repo

---
+++ GameEngine/src/tests/engine/importers/test_mesh_importer.cpp
@@ -0,0 +1,16 @@
+// std 
+#include <iostream>
+#include <iterator>
+#include <algorithm>
+
+// Gtest
+#include <gtest/gtest.h>
+
+// local 
+
+
+TEST(TestEngineImporters, TestMeshImporter) 
+{    
+    EXPECT_TRUE(true);
+}
+
---
+++ GameEngine/src/tests/engine/resources/test_mesh_serializer.cpp
@@ -0,0 +1,16 @@
+// std 
+#include <iostream>
+#include <iterator>
+#include <algorithm>
+
+// Gtest
+#include <gtest/gtest.h>
+
+// local 
+
+
+TEST(TestEngineResources, TestMeshSerializer) 
+{    
+    EXPECT_TRUE(true);
+}
+
