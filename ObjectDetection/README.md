6aa2ef0271fa003ebfb49a0ae1271fc7c23c7e5f
Jonathan Dorsey <jdorsey43@gatech.edu>
2026-06-05 21:34:29 -0700

    noodling.... without focus.... basically just iterator review

---
+++ ObjectDetection/README.md
@@ -0,0 +1,7 @@
+# Object Detection From Scratch
+
+## Goals 
+- [ ] Database from Scratch (file-based Sqlite-ish)
+- [ ] HTTP client/server from scratch 
+- [ ] Use OpenCV (if I have to) to get images from a web camera running on a raspberry pi 
+- [ ] 
---
+++ ObjectDetection/tests/test_dummy_UT.cpp
@@ -0,0 +1,16 @@
+// GTest
+#include <gtest/gtest.h>
+
+// std 
+#include <iostream>
+#include <format>
+#include <string>
+#include <vector>
+
+/*********************/
+/*      Tests      */
+/*********************/
+TEST(TestDummyTest, TestDummy)
+{
+    EXPECT_TRUE(true);
+}
